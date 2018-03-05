5a49fb70660be37bf2064b9d52a7921a2b691dae
Erul <erulggubas@yahoo.com>
2018-03-06 06:55:17 +0800

    modified app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,67 @@
 import { Component } from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import { Observable } from '@firebase/util/dist/esm/src/subscribe';
-import { PartialObserver } from 'rxjs/Observer';
 
 interface Post {
-FirstName : string;
-LastName:string;
-Phone :string;
-Mobile :string;
-Email :string;
-Address :string;
-
+  firstname: string;
+  lastname: string;
+  phone: string;
+  mobile: string;
+  email: string;
+  address: string;
 }
+
+interface PostId extends Post { 
+  id: string; 
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-posts :any;
+  
+
   postsCol: AngularFirestoreCollection<Post>;
-  post: Observable<Post[]>;
+  posts: any;
 
-  constructor(private afs: AngularFirestore){
+  firstname: string;
+  lastname: string;
+  phone: string;
+  mobile: string;
+  email: string;
+  address: string;
 
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit() {
+    this.postsCol = this.afs.collection('posts');
+    this.posts = this.postsCol.snapshotChanges()
+      .map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+          return { id, data };
+        })
+      })
 
   }
-ngOnInit(){
-  this.postsCol = this.afs.collection('posts');
-  this.posts = this.postsCol.valueChanges();
+  addPost() {
+    this.afs.collection('posts').doc('my-custom-id').set({'firstname': this.firstname, 'lastname': this.lastname,
+  'phone': this.phone, 'mobile': this.mobile, 'email': this.email, 'address': this.address});
+  }
 
+  getPost(postId) {
+    this.postsCol = this.afs.collection('posts', ref => ref.where('firstname', '==', 'Erul'));
+  }
+  deletePost(postId) {
+    this.afs.doc('posts/'+postId).delete();
+  }
 }
-}
+
