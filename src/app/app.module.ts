b77ffa7b2b5e07e0556f6998605184e816342d3b
Fritz Hulguin <hulguinf@gmail.com>
2018-03-05 14:48:09 +0800

    modified appmodule.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import {FormsModule} from '@angular/forms';
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBbRBtapTtPv6MSZjB2PJnzawjnxzCXJXs",
+  authDomain: "firestore-309b7.firebaseapp.com",
+  databaseURL: "https://firestore-309b7.firebaseio.com",
+  projectId: "firestore-309b7",
+  storageBucket: "",
+  messagingSenderId: "8912526197"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
