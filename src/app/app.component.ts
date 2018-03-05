36489930f5923b7478343d504a34acd1b0354aea
Fritz Hulguin <hulguinf@gmail.com>
2018-03-05 15:06:12 +0800

    modified app component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import 'rxjs/add/operator/map';
+import { Observable } from '@firebase/util/dist/esm/src/subscribe';
 
+interface Post {
+FirstName : string;
+LastName:string;
+Phone :string;
+Mobile :string;
+Email :string;
+Address :string;
+
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: Observable<Post[]>;
+
+  constructor(private afs: AngularFirestore){
+
+
+  }
+ngOnInit(){
+  this.postsCol = this.afs.collection('posts');
+  this.posts= this.postsCol.valueChanges();
+
+}
 }
